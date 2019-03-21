abcb73d84b0e93d283eccbcb535f8e2c7cff928e
dJOKERdUMMY <avineshkumarcpl@gmail.com>
2019-03-21 20:24:33 +0530

    Reading files as text, not byte

--- caption_generator/prepare_dataset.py
+++ caption_generator/prepare_dataset.py
@@ -28,25 +28,25 @@ def get_encoding(model, img):
 	return pred
 
 def prepare_dataset(no_imgs = -1):
-	f_train_images = open('Flickr8k_text/Flickr_8k.trainImages.txt','rb')
-	train_imgs = f_train_images.read().strip().split(b'\n') if no_imgs == -1 else f_train_images.read().strip().split('\n')[:no_imgs]
+	f_train_images = open('Flickr8k_text/Flickr_8k.trainImages.txt','r')
+	train_imgs = f_train_images.read().strip().split('\n') if no_imgs == -1 else f_train_images.read().strip().split('\n')[:no_imgs]
 	f_train_images.close()
 
-	f_test_images = open('Flickr8k_text/Flickr_8k.testImages.txt','rb')
-	test_imgs = f_test_images.read().strip().split(b'\n') if no_imgs == -1 else f_test_images.read().strip().split('\n')[:no_imgs]
+	f_test_images = open('Flickr8k_text/Flickr_8k.testImages.txt','r')
+	test_imgs = f_test_images.read().strip().split('\n') if no_imgs == -1 else f_test_images.read().strip().split('\n')[:no_imgs]
 	f_test_images.close()
 
-	f_train_dataset = open('Flickr8k_text/flickr_8k_train_dataset.txt','wb')
-	f_train_dataset.write(b"image_id\tcaptions\n")
+	f_train_dataset = open('Flickr8k_text/flickr_8k_train_dataset.txt','w')
+	f_train_dataset.write("image_id\tcaptions\n")
 
-	f_test_dataset = open('Flickr8k_text/flickr_8k_test_dataset.txt','wb')
-	f_test_dataset.write(b"image_id\tcaptions\n")
+	f_test_dataset = open('Flickr8k_text/flickr_8k_test_dataset.txt','w')
+	f_test_dataset.write("image_id\tcaptions\n")
 
-	f_captions = open('Flickr8k_text/Flickr8k.token.txt', 'rb')
-	captions = f_captions.read().strip().split(b'\n')
+	f_captions = open('Flickr8k_text/Flickr8k.token.txt', 'r')
+	captions = f_captions.read().strip().split('\n')
 	data = {}
 	for row in captions:
-		row = row.split(b"\t")
+		row = row.split("\t")
 		row[0] = row[0][:len(row[0])-2]
 		try:
 			data[row[0]].append(row[1])
@@ -76,7 +76,7 @@ def prepare_dataset(no_imgs = -1):
 			f_test_dataset.flush()
 			c_test += 1
 	f_test_dataset.close()
-	with open( "encoded_images.p", "wb" ) as pickle_f:
+	with open( "encoded_images.p", "w" ) as pickle_f:
 		pickle.dump( encoded_images, pickle_f )  
 	return [c_train, c_test]
 
